8a5f912851c347e69daba8fc9b9c5bea703f40d1
Alexandra Roshchina <sasharo@mail.ru>
2018-02-16 17:49:20 +0300

    create threads

--- src/main/java/lesson_16_02_2018/Race.java
+++ src/main/java/lesson_16_02_2018/Race.java
@@ -1,16 +1,34 @@
 package lesson_16_02_2018;
 
+import sun.awt.geom.AreaOp;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 public class Race {
 
-    public static void main(String[] args) {
+    private static final int CARS_COUNT = 5;
 
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch1 = new CountDownLatch(CARS_COUNT);
+
+        for (int i = 0; i < CARS_COUNT; i++) {
+            new Thread(new Car(ThreadLocalRandom.current().nextInt(), String.valueOf("Car-" + (i + 1))))
+                .start();
+        }
+
+        System.out.println("Ready");
+        TimeUnit.SECONDS.sleep(1);
     }
 
-    private class Car implements Runnable {
-        public float speed = 0;
+    private static class Car implements Runnable {
+        public float speed;
+        public String name;
 
-        public Car(float speed) {
+        public Car(float speed, String name) {
             this.speed = speed;
+            this.name = name;
         }
 
         @Override
